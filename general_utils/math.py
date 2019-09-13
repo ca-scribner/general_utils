88a2fbf44c9c8e6dde58766cf67e6806ca48f76f
Andrew Scribner <andrew.scribner@canada.ca>
2019-09-13 13:09:55 -0400

    Update formatting in math module

--- general_utils/math.py
+++ general_utils/math.py
@@ -1,11 +1,13 @@
 import numpy as np
 
+
 def polar_to_cart(r, theta, unit='deg'):
     if unit == 'deg':
         theta = np.deg2rad(theta)
     x = r * np.cos(theta)
     y = r * np.sin(theta)
     return x, y
+
 
 def cart_to_polar(x, y, unit='deg'):
     r = np.sqrt(x**2 + y**2)
