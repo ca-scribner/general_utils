370ba890f4f7379edb495005183eca143149e862
Andrew Scribner <andrew.scribner@canada.ca>
2019-09-20 13:47:45 -0400

    Add Timer() class for a lightweight timer

--- general_utils/misc.py
+++ general_utils/misc.py
@@ -1,6 +1,6 @@
 import collections
 import six
-
+import time
 
 def is_iterable(arg):
     """
@@ -18,3 +18,27 @@ def is_iterable(arg):
         isinstance(arg, collections.Iterable)
         and not isinstance(arg, six.string_types)
         )
+
+
+class Timer:
+    def __init__(self):
+        self.reference_time = None
+        self.reset()
+
+    def elapsed(self):
+        """
+        Return the time elapsed between when this object was instantiated (or last reset) and now
+
+        Returns:
+            (float): Time elapsed in seconds
+        """
+        return time.perf_counter() - self.reference_time
+
+    def reset(self):
+        """
+        Reset the reference timepoint to now
+
+        Returns:
+            None
+        """
+        self.reference_time = time.perf_counter()
